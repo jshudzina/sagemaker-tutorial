5ce953a391e1b7e132a8b8c15efc92ddb7ff7710
EC2 Default User <ec2-user@ip-172-16-137-3.ec2.internal>
2018-06-03 01:06:05 +0000

    updated to save a vocab file

--- glue/tokenize.scala
+++ glue/tokenize.scala
@@ -2,6 +2,7 @@ import com.amazonaws.services.glue.GlueContext
 import com.amazonaws.services.glue.util.GlueArgParser
 import com.amazonaws.services.glue.util.Job
 import org.apache.spark.SparkContext
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 
 import org.apache.spark.sql.functions._
 
@@ -33,6 +34,7 @@ object Tokenizer {
 
     val filtered_df = df.filter(df("commentBody").isNotNull)
 
+    //Tokenize and filter stop words
     val regexTokenizer = new RegexTokenizer()
     .setInputCol("commentBody")
     .setOutputCol("words")
@@ -47,26 +49,42 @@ object Tokenizer {
 
     val removed_df = remover.transform(tokenized_df)
 
+    //Create a vocabulary 
     val cvModel: CountVectorizerModel = new CountVectorizer()
         .setInputCol("filtered_words")
         .setOutputCol("features")
         .setVocabSize(1000)
         .setMinDF(5)
         .fit(removed_df)
+    
+      
+    //Save off the vocabulary for lambda functions...
+    val json_vacab = cvModel
+        .vocabulary
+        .toSeq
+        .zipWithIndex
+        .map {
+            case (word, index) => s""""${word}":${index}"""
+        }
+        .mkString("{\n",",\n","\n}")
+        
+    val s3 = AmazonS3ClientBuilder.defaultClient()
+    s3.putObject(bucket,"data/nyt-features/vocab.json",json_vacab)
 
+    //Convert tokens to a bag-of-words
     val feature_df = cvModel.transform(removed_df)
 
     val labeled_df = feature_df
         .select("articleID", "commentID", "commentBody", "features")
         .withColumn("labels", lit(0.0))
 
-    labeled_df.show()
-
+    //Save labeled data for Athena
     labeled_df
         .write
         .mode("overwrite")
         .parquet(s"s3://${bucket}/data/nyt-features/labeled_data.parquet")
 
+    //Save labeled data for sagemaker in Record IO format
     labeled_df
         .coalesce(1)
         .write
